4c5253278064d20fa244b43db0ed5e36b9984fe0
Zach Nation <znation@gmail.com>
2011-09-28 23:36:14 -0700

    2nd attempt at ball-ball bouncing, still not working

--- puck.js
+++ puck.js
@@ -69,12 +69,16 @@ function Stick(player)
     this.nextPosition = this.position;
     this.color = player.color;
     this.angle = 0;
+    this.speed = 0;
+    this.mass = 1000;
 }
 Stick.prototype = new Circle;
 Stick.prototype.updatePosition = function(x, y) {
     this.nextPosition = new Vector(x, y);
 };
 Stick.prototype.move = function() {
+    this.speed = this.nextPosition.distance(this.position);
+    this.angle = this.position.subtract(this.nextPosition).toAngle();
     this.position = this.nextPosition;
 };
 
@@ -105,6 +109,7 @@ function Puck()
     this.angle = Math.random() * Math.PI * 2;
     this.speed = 5;
     this.color = "rgb(0,255,255)";
+    this.mass = 1;
 }
 Puck.prototype = new Circle;
 Puck.prototype.detectCollisions = function() {
@@ -144,12 +149,36 @@ Puck.prototype.detectCollisions = function() {
         if (d <= this.radius + stick.radius)
         {
             // Collision detected
+            // Algorithm from http://www.vobarian.com/collisions/2dcollisions2.pdf
             
-            
-            var v = this.position.subtract(stick.position);
-            var va = normalizeAngle(v.toAngle());
-            var vb = va; // turn by 90 degrees
-            this.angle = normalizeAngle(vb);
+            var n = this.position.subtract(stick.position);
+            var un = n.normalize();
+            var ut = new Vector(-un.y, un.x);
+
+            var m1 = this.mass;
+            var m2 = stick.mass;
+
+            var v1 = new Vector(Math.sin(this.angle), Math.cos(this.angle)).multiply(this.speed);
+            var v2 = new Vector(Math.sin(stick.angle), Math.cos(stick.angle)).multiply(stick.speed);
+
+            var v1n = un.dot(v1);
+            var v1t = ut.dot(v1);
+            var v2n = un.dot(v2);
+            var v2t = ut.dot(v2);
+
+            var vP1t = v1t, vP2t = v2t;
+            var vP1n = ((v1n * (m1 - m2)) + (2 * (m2 * v2n))) / (m1 + m2);
+            var vP2n = ((v2n * (m2 - m1)) + (2 * (m1 * v1n))) / (m1 + m2);
+
+            var VP1n = un.multiply(vP1n);
+            var VP1t = ut.multiply(vP1t);
+            var VP2n = un.multiply(vP2n);
+            var VP2t = ut.multiply(vP2t);
+
+            var VP1 = VP1n.add(VP1t);
+            var VP2 = VP2n.add(VP2t);
+
+            this.angle = Math.atan2(VP1.x, VP1.y);
         }
     }
 };
