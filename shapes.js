a1d5fc893e47190587e69940d892b441620589c1
Zach Nation <znation@gmail.com>
2011-10-01 16:18:14 -0700

    Draw sticks as concentric circles

--- shapes.js
+++ shapes.js
@@ -49,6 +49,27 @@ Stick.prototype.move = function() {
     this.position = this.nextPosition;
     this.boxMouseJoint.SetTarget(new b2Vec2(pixelToBoxCoords(this.position.x), pixelToBoxCoords(this.position.y)));
 };
+Stick.prototype.draw = function(ctx) {
+    var innerCircleWidth = Math.floor(this.radius / 10.0);
+    var outerCircleWidth = Math.floor(this.radius / 15.0);
+    innerCircleWidth = Math.max(1, innerCircleWidth);
+    outerCircleWidth = Math.max(innerCircleWidth+1, outerCircleWidth);
+    var innerCircleRadius = this.radius / 2;
+
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = outerCircleWidth;
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, true); 
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = innerCircleWidth;
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, innerCircleRadius, 0, Math.PI*2, true); 
+    ctx.closePath();
+    ctx.stroke();
+};
 
 function Puck()
 {
