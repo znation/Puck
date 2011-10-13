4b77eae3178e8a17a54f00fd592b4ae28ba0b09d
Zach Nation <znation@gmail.com>
2011-10-12 21:45:44 -0700

    Make short-score game debug-only

--- Utility.h
+++ Utility.h
@@ -3,7 +3,12 @@
 
 #pragma once
 
+#if DEBUG
 #define MAX_SCORE 2
+#else
+#define MAX_SCORE 11
+#endif
+
 double pixelToBox(int coord);
 int boxToPixel(double coord);
 bool rectContainsPoint(D2D1_RECT_F rect, b2Vec2 point);
